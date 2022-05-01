6e70c5696a834fc047f73f83d0757a930869cc7c
Pratyush Mittal <pratyushmittal@gmail.com>
2022-05-02 01:13:51 +0530

    The hook didn't work as intented. Echoing commands for manual run.

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-pip install -r requirements/requirements.txt
-pre-commit install
+echo "Project setup compalete"
+echo "run these commands"
+echo "cd {{cookiecutter.project_name}}"
+echo "pip install -r requirements/requirements.txt"
+echo "pre-commit install"
