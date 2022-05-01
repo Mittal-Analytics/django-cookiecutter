b1759584002cd532db29b93286af4b4a8e3f57a9
Pratyush Mittal <pratyushmittal@gmail.com>
2022-05-01 20:04:42 +0530

    Fixed placeholders

--- {{cookiecutter.project_slug}}/deploy/uberspace/install.sh
+++ {{cookiecutter.project_slug}}/deploy/uberspace/install.sh
@@ -1,15 +1,15 @@
 # copy the code directory to remote
 # the run following commands
-echo "run on local\n$ scp -r deploy getting-things-done:"
+echo "run on local\n$ scp -r deploy {{cookiecutter.project_slug}}:"
 cd deploy/uberspace
 
 set -e
 
 # create folders
 mkdir ~/repos
-git init --bare ~/repos/gtd.git
-cp ../post-receive ~/repos/gtd.git/hooks/post-receive
-chmod +x ~/repos/gtd.git/hooks/post-receive
+git init --bare ~/repos/{{cookiecutter.project_slug}}.git
+cp ../post-receive ~/repos/{{cookiecutter.project_slug}}.git/hooks/post-receive
+chmod +x ~/repos/{{cookiecutter.project_slug}}.git/hooks/post-receive
 mkdir ~/webapps
 
 # https://lab.uberspace.de/guide_django.html
@@ -25,26 +25,26 @@ supervisorctl update
 supervisorctl status
 
 # configure web server
-uberspace web domain add infur.in
-uberspace web backend set infur.in --http --port 8000
+uberspace web domain add {{cookiecutter.domain_name}}
+uberspace web backend set {{cookiecutter.domain_name}} --http --port 8000
 
 # create deamon
 mkdir -p ~/uwsgi/apps-enabled/
 cp django-app.ini ~/uwsgi/apps-enabled/
 
 # configure static servers
-uberspace web backend set infur.in/static --apache
-uberspace web backend set infur.in/media --apache
+uberspace web backend set {{cookiecutter.domain_name}}/static --apache
+uberspace web backend set {{cookiecutter.domain_name}}/media --apache
 
 # add nginx headers
-uberspace web header set infur.in/static/ expires 7d
-uberspace web header set infur.in/favicon.ico root /var/www/virtual/getdone/html/static/favicons
-uberspace web header set infur.in/favicon.ico expires 7d
+uberspace web header set {{cookiecutter.domain_name}}/static/ expires 7d
+uberspace web header set {{cookiecutter.domain_name}}/favicon.ico root /var/www/virtual/{{cookiecutter.uberspace_login}}/html/static/favicons
+uberspace web header set {{cookiecutter.domain_name}}/favicon.ico expires 7d
 
-uberspace web header set infur.in/static gzip on
-uberspace web header set infur.in/static gzip_comp_level 6
-uberspace web header set infur.in/static gzip_types text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml
-uberspace web header set infur.in/static gzip_types "text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml"
+uberspace web header set {{cookiecutter.domain_name}}/static gzip on
+uberspace web header set {{cookiecutter.domain_name}}/static gzip_comp_level 6
+uberspace web header set {{cookiecutter.domain_name}}/static gzip_types text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml
+uberspace web header set {{cookiecutter.domain_name}}/static gzip_types "text/plain text/css text/xml application/json application/javascript application/xml+rss application/atom+xml image/svg+xml"
 
 
 # import content
