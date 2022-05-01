2e3d742ca99b99c340eb98e7ef6c7687d3b76ec0
Pratyush Mittal <pratyushmittal@gmail.com>
2022-05-01 16:54:56 +0530

    Synced changes

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/urls.py
@@ -17,8 +17,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    # render index.html on /
+    path("", TemplateView.as_view(template_name="index.html")),
     path("admin/", admin.site.urls),
 ]
 
