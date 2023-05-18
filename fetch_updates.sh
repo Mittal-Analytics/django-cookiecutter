7643f38a7608610655932d04401385e0b05a9f99
Pratyush Mittal <pratyushmittal@gmail.com>
2023-05-18 10:25:01 +0530

    Updated fetch_updates

--- fetch_updates.sh
+++ fetch_updates.sh
@@ -1,16 +1,20 @@
 # exit on error
 set -e
 
-echo "Fetching updates..."
+# keep the argument same if possible
+folder_path="$1"
+folder_name="$(basename "$folder_path")"
+echo "Fetching updates from $folder_name"
+
 git checkout raw-target
-rsync -av ../getting_things_done ./ --exclude=.venv --exclude=.git --exclude=db.sqlite3 --exclude='*.pyc' --exclude="__pycache__" --exclude=".DS_Store"
-find getting_things_done -type f | xargs sed -i '' 's/getting_things_done/{{cookiecutter.project_slug}}/g'
-mv getting_things_done/getting_things_done getting_things_done/"{{cookiecutter.project_slug}}"
+rsync -av "$folder_path" ./ --exclude=.venv --exclude=.git --exclude=db.sqlite3 --exclude='*.pyc' --exclude="__pycache__" --exclude=".DS_Store"
+find "$folder_name" -type f | xargs grep -Il "" | xargs sed -i '' "s/$folder_name/{{cookiecutter.project_slug}}/g"
+mv "$folder_name/$folder_name" "$folder_name/{{cookiecutter.project_slug}}"
 mv "{{cookiecutter.project_slug}}" backup_"{{cookiecutter.project_slug}}"
-mv getting_things_done "{{cookiecutter.project_slug}}"
+mv "$folder_name" "{{cookiecutter.project_slug}}"
 rm -rf backup_"{{cookiecutter.project_slug}}"
 git add . -A
-git commit -m "Synced changes from getting_things_done"
+git commit -m "Synced changes from $folder_name"
 
 
 echo "triaging changes..."
