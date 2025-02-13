8a4355fd4832aa6a6e01f0c9004c4a3d69d37d77
Pratyush Mittal <pratyushmittal@gmail.com>
2025-02-13 11:17:07 +0530

    Synced changes from stock_chat

--- {{cookiecutter.project_slug}}/manage.py
+++ {{cookiecutter.project_slug}}/manage.py
@@ -7,9 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault(
-        "DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings.local"
-    )
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{cookiecutter.project_slug}}.settings.local")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
